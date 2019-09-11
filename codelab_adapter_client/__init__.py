9d7dc92ce5a1ae388e05c24ec7869d16730df6f6
wwj718 <wuwenjie718@gmail.com>
2019-09-11 12:23:03 +0800

    upgrade(0.9.8)

--- codelab_adapter_client/__init__.py
+++ codelab_adapter_client/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Wenjie Wu"""
 __email__ = 'wuwenjie718@gmail.com'
-__version__ = '0.9.4'
+__version__ = '0.9.8'
 
 from .base import MessageNode, AdapterNode, JupyterNode, SimpleNode
 from .hass import HANode
