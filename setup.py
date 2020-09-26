6c7e6a469ecbc47087ff612af18bf7b026ce5dda
wwj718 <wuwenjie718@gmail.com>
2020-09-26 11:26:47 +0800

    update to 2.0.1

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/wwj718/codelab_adapter_client',
-    version='2.0.0',
+    version='2.0.1',
     zip_safe=False,
 )
