0a5e87968d474df8755d4e74e7e613e229e6e254
wwj718 <wuwenjie718@gmail.com>
2021-05-14 11:35:07 +0800

    update

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/wwj718/codelab_adapter_client',
-    version='4.2.3',
+    version='4.3.0',
     zip_safe=False,
 )
