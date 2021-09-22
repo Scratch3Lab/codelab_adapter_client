bd07a402830f7533a40927211a31e2910596dbed
wwj718 <wuwenjie718@gmail.com>
2021-09-22 16:12:47 +0800

    4.4.1

--- codelab_adapter_client/__init__.py
+++ codelab_adapter_client/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Wenjie Wu"""
 __email__ = 'wuwenjie718@gmail.com'
-__version__ = '4.4.0'
+__version__ = '4.4.1'
 
 from .base import MessageNode, AdapterNode, JupyterNode, SimpleNode
 from .hass import HANode
